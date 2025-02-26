16957ae708e8fecf1f0d179d09f804eb3fe1cdea
kimwonjoon_Mac <1085168149@qq.com>
2025-02-26 16:15:11 +0800

    feat: init project

---
+++ open-code-review-sdk/src/test/java/top/kimwonjoon/sdk/test/ApiTest.java
@@ -0,0 +1,15 @@
+package top.kimwonjoon.sdk.test;
+
+/**
+ * @ClassName ApiTest
+ * @Description
+ * @Author @kimwonjoon
+ * @Date 2025/2/26 15:41
+ * @Version 1.0
+ */
+
+public class ApiTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ openai-code-review-test/src/main/java/top/kimwonjoon/Application.java
@@ -0,0 +1,12 @@
+package top.kimwonjoon;
+
+/**
+ * @ClassName Application
+ * @Description
+ * @Author @kimwonjoon
+ * @Date 2025/2/26 16:10
+ * @Version 1.0
+ */
+
+public class Application {
+}

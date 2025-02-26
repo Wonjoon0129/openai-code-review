611283848d578a4a3f45aeac5ea5469fd6c84ca3
kimwonjoon_Mac <1085168149@qq.com>
2025-02-26 23:12:03 +0800

    feat :v1

--- open-code-review-sdk/src/main/java/top/kimwonjoon/sdk/OpenAiCodeReview.java
+++ open-code-review-sdk/src/main/java/top/kimwonjoon/sdk/OpenAiCodeReview.java
@@ -1,15 +1,5 @@
 package top.kimwonjoon.sdk;
 
-
-import com.alibaba.dashscope.aigc.generation.Generation;
-import com.alibaba.dashscope.aigc.generation.GenerationParam;
-import com.alibaba.dashscope.aigc.generation.GenerationResult;
-import com.alibaba.dashscope.common.Message;
-import com.alibaba.dashscope.common.Role;
-import com.alibaba.dashscope.exception.ApiException;
-import com.alibaba.dashscope.exception.InputRequiredException;
-import com.alibaba.dashscope.exception.NoApiKeyException;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -18,21 +8,6 @@ import java.util.Arrays;
 
 public class OpenAiCodeReview {
 
-    public static GenerationResult callWithMessage(String message) throws ApiException, NoApiKeyException, InputRequiredException {
-        Generation gen = new Generation();
-        Message userMsg = Message.builder()
-                .role(Role.USER.getValue())
-                .content(message)
-                .build();
-        GenerationParam param = GenerationParam.builder()
-                // 若没有配置环境变量，请用百炼API Key将下行替换为：.apiKey("sk-xxx")
-                .apiKey("sk-db2968be21644311a3ceca2de967552b")
-                .model("deepseek-r1")
-                .messages(Arrays.asList(userMsg))
-                .resultFormat(GenerationParam.ResultFormat.MESSAGE)
-                .build();
-        return gen.call(param);
-    }
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("测试执行");
 
@@ -52,18 +27,6 @@ public class OpenAiCodeReview {
         int exitCode = process.waitFor();
         System.out.println("ExitCode:"+exitCode);
         System.out.println("评审代码"+diffCode.toString());
-
-        //2.通过 deepseek 代码评审
-        try {
-            GenerationResult result = callWithMessage(diffCode.toString());
-            System.out.println("思考过程：");
-            System.out.println(result.getOutput().getChoices().get(0).getMessage().getReasoningContent());
-            System.out.println("回复内容：");
-            System.out.println(result.getOutput().getChoices().get(0).getMessage().getContent());
-        } catch (ApiException | NoApiKeyException | InputRequiredException e) {
-            // 使用日志框架记录异常信息
-            System.err.println("An error occurred while calling the generation service: " + e.getMessage());
-        }
 
     }
 }

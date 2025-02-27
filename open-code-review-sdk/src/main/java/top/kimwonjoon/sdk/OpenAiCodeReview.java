5ba1e6107505e8e96b3eab02be5582e3300537be
kimwonjoon_Mac <1085168149@qq.com>
2025-02-27 15:57:58 +0800

    feat :test_v5

--- open-code-review-sdk/src/main/java/top/kimwonjoon/sdk/OpenAiCodeReview.java
+++ open-code-review-sdk/src/main/java/top/kimwonjoon/sdk/OpenAiCodeReview.java
@@ -56,7 +56,7 @@ public class OpenAiCodeReview {
         connection.setDoOutput(true);
 
         ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
-        chatCompletionRequest.setModel(Model.DEEPSEEK_R1_DISTILL_QWEN_1_5B.getCode());
+        chatCompletionRequest.setModel(Model.DEEPSEEK_V3.getCode());
         chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
             {
                 add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
